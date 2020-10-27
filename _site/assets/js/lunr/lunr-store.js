9fa88ae83147acc31bfe33c773e15a89590278c2
양경모 <fover32@gmail.com>
2020-10-28 02:04:03 +0900

    year archive

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,12 +1,12 @@
 var store = [{
-        "title": "[UIKit] 내 iOS 프로젝트를 Interface Builder 없이 만들어보자!",
+        "title": "UIKit) 내 iOS 프로젝트를 Interface Builder 없이 만들어보자!",
         "excerpt":"안녕하세요! 갱입니다. iOS 개발을 주제로 블로그를 하게 됐습니다. 😆 저는 소프트웨어 마에스트로 11기에서 iOS 네이티브 앱 개발을 공부하고 있는 중입니다. 전부터 iOS 개발을 하고있었지만 항상 IB를 이용해서 앱 개발을 했었지만 이번 소마 프로젝트에서는 모두 코드로만 개발하기를 시작했습니다. IB를 이용해서 개발하다가 코드로만 하니 제가 자료 검색을 못하는 건지 내용이 많이 없는...","categories": ["Blog"],
         "tags": ["Blog"],
         "url": "http://localhost:4000/blog/first-post/",
         "teaser": "http://localhost:4000/git/assets/images/memoji.png"
       },{
-        "title": "[UIKit] UITabBarController를 코드로 만들어보자!",
-        "excerpt":"안녕하세요 갱입니다. 🤔   네이버에서 Github Pages로 옮겼는데 그 이유는 커밋을 좀 더 자주하기 위해서…?   소마 프로젝트가 막바지로 들어가고 있는데 저는 이제 60퍼 정도 완성되어가고 있어요…   ","categories": ["Blog"],
+        "title": "UIKit) UITabBarController를 코드로 만들어보자!",
+        "excerpt":"안녕하세요 갱입니다. 🤔 네이버에서 Github Pages로 옮겼는데 그 이유는 커밋을 좀 더 자주하기 위해서…? 소마 프로젝트가 막바지로 들어가고 있는데 저는 이제 60퍼 정도 완성되어가고 있어요… 코로나때문에 센터에는 몇번 가보지도 못하고… 센터 책상을 써본게 손에 꼽을 정도에요… 이렇게 저의 소마 생활이 끝나게 될 것 같아요… 다음 12기 연수생분들은 꼭 센터를 사용할...","categories": ["Blog"],
         "tags": ["Blog"],
         "url": "http://localhost:4000/blog/tabbar/",
         "teaser": "http://localhost:4000/git/assets/images/memoji.png"
