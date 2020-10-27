4bf90d412951045c0db6ce273d825cab68cb9606
양경모 <fover32@gmail.com>
2020-10-27 16:32:58 +0900

    info change

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -3,5 +3,5 @@ var store = [{
         "excerpt":"안녕하세요! 갱입니다. iOS 개발을 주제로 블로그를 하게 됐습니다. 😆 저는 소프트웨어 마에스트로 11기에서 iOS 네이티브 앱 개발을 공부하고 있는 중입니다. 전부터 iOS 개발을 하고있었지만 항상 IB를 이용해서 앱 개발을 했었지만 이번 소마 프로젝트에서는 모두 코드로만 개발하기를 시작했습니다. IB를 이용해서 개발하다가 코드로만 하니 제가 자료 검색을 못하는 건지 내용이 많이 없는...","categories": ["Blog"],
         "tags": ["Blog"],
         "url": "http://localhost:4000/blog/first-post/",
-        "teaser": "http://localhost:4000/assets/images/Macintosh_128k_transparency.png"
+        "teaser": "http://localhost:4000/assets/images/memoji.png"
       }]
